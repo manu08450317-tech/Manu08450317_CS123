787e6f427aa62c3f392cf498637d4bc6cb5a60ac
manu08450317-tech <manu08450317@gmail.com>
2026-04-18 22:55:25 +0530

    Add files via upload

---
+++ JListExample.java
@@ -0,0 +1,55 @@
+package Swing;
+
+import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class JListExample extends JFrame {
+    private JList<String> countryList;
+
+    public JListExample() {
+        // Create the model and add elements
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listModel.addElement("USA");
+        listModel.addElement("India");
+        listModel.addElement("Vietnam");
+        listModel.addElement("Canada");
+        listModel.addElement("Denmark");
+        listModel.addElement("France");
+        listModel.addElement("Great Britain");
+        listModel.addElement("Japan");
+        listModel.addElement("Africa");
+        listModel.addElement("Greenland");
+        listModel.addElement("Singapore");
+
+        // Create the list
+        countryList = new JList<>(listModel);
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    final List<String> selectedValuesList = countryList.getSelectedValuesList();
+                    System.out.println("Selected: " + selectedValuesList);
+                }
+            }
+        });
+
+        // Add list inside a scroll pane
+        add(new JScrollPane(countryList));
+
+        // Frame settings
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("JList Example");
+        this.setSize(250, 250);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JListExample();
+    }
+}
