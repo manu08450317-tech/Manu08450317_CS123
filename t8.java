6fd043ee153e761c2e77b36eaa43b62a9b0a2a37
manu08450317-tech <manu08450317@gmail.com>
2026-04-19 16:59:18 +0530

    Delete t8.java

--- t8.java
+++
@@ -1,9 +0,0 @@
-Q8. Write a Java Program for Verifying if a string contains only numeric characters using user defined
-function isNumeric()
-  // 8. Function to check if a string contains only digits
-public static boolean isNumeric(String str) {
-if (str == null || str.isEmpty()) {
-return false;
-}
-return str.matches(&quot;\\d+&quot;);
-}
