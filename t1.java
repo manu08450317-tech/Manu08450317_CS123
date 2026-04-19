76e121fbee6f4e37f64d67d133ae79a8f3635416
manu08450317-tech <manu08450317@gmail.com>
2026-04-19 16:57:45 +0530

    Delete t1.java

--- t1.java
+++
@@ -1,7 +0,0 @@
-Q1. Write a Java Program for Checking if a given string is null or contains only whitespace
-using user defined function isNullOrEmpty().
-public class StringBuildingFunctions {
-// 1. Function to check if a string is empty or null
-public static boolean isNullOrEmpty(String str) {
-return str == null || str.trim().isEmpty();
-}
