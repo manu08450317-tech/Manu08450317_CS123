012540e8f328b4fafb4777b676a77c7725da90d7
manu08450317-tech <manu08450317@gmail.com>
2026-04-19 16:58:42 +0530

    Delete t5.java

--- t5.java
+++
@@ -1,6 +0,0 @@
-Q5. Write a Java Program for Eliminating all whitespace characters from a string using user defined
-function removeWhitespace()
-// 5. Function to remove all whitespace from a string
-public static String removeWhitespace(String str) {
-return str == null ? null : str.replaceAll(&quot;\\s&quot;, &quot;&quot;);
-}
